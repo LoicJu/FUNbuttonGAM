ae1c6eb84f2bfce247caa64ae672e25f92fd77ca
loic.jurasz <loic.jurasz@he-arc.ch>
2020-08-11 14:06:08 +0200

    bonjour

---
+++ Assets/code/Game/SelectPerson.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPerson : MonoBehaviour
+{
+
+    public List<GameObject> listCircle;
+    public GameObject attributes;
+    public GameObject attributesPerson;
+    private float x_person;
+    // Start is called before the first frame update
+    void Start()
+    {
+        listCircle = GameObject.Find("Generation").GetComponent<ShowPerson>().circlesList;
+        attributesPerson = Instantiate(attributes, new Vector3(100, 100, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //timer
+        // select person
+        GameObject selectedCircle = listCircle[0];
+        // generate personage
+        // todo change text
+        attributesPerson.transform.position = new Vector3(selectedCircle.transform.position.x, selectedCircle.transform.position.y, 0);
+    }
+}
--- Assets/code/Game/ShowPerson.cs
+++ Assets/code/Game/ShowPerson.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 
 public class ShowPerson : MonoBehaviour
 {
+    public List<GameObject> circlesList;
     public GameObject circle;
+    public float time = 0.0f;
+    public float WAIT_TIME = 2.0f;
+    private int NB_INSTANCES = 4;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(circle, new Vector3(0.208757f, -0.2164043f, -6.712757f), Quaternion.identity);
+        for(int i=0; i< NB_INSTANCES; i++)
+        {
+            Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+            float x = Random.Range(-screenBounds.x, screenBounds.x);
+            float y = Random.Range(-screenBounds.y, screenBounds.y);
+            GameObject instance = Instantiate(circle, new Vector3(x, y, 0), Quaternion.identity);
+            circlesList.Add(instance);
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        if(time > WAIT_TIME)
+        {
+            time -= WAIT_TIME;
+            // screenBounds not const in case of resize
+            Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+            float x = Random.Range(-screenBounds.x, screenBounds.x);
+            float y = Random.Range(-screenBounds.y, screenBounds.y);
+            
+            GameObject instance = Instantiate(circle, new Vector3(x, y, 0), Quaternion.identity);
+            circlesList.Add(instance);
+            if (circlesList.Count > NB_INSTANCES)
+            {
+                Destroy(circlesList[0]);
+                circlesList.RemoveAt(0);
+            }
+        }
+
     }
 }
