c8b1e4ad94a4e1aba76e98feb8e467c0e5f14e99
VincentBeguin <69506257+VincentBeguin@users.noreply.github.com>
2020-08-11 14:09:56 +0200

    Pour tests

---
+++ Assets/generation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class generation : MonoBehaviour
+{
+	string path;
+	string jsonString;
+		// Start is called before the first frame update
+		void Start()
+		{
+			path = Application.dataPath + "/application.json";
+			jsonString = File.ReadAllText(path);
+			Debug.Log(jsonString);
+			AttribList list = JsonUtility.FromJson<AttribList> (jsonString);
+			Debug.Log(list);
+		}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
+
+[System.Serializable]
+public class Attribute
+{
+	public string nom;
+	public int score;
+}
+
+[System.Serializable]
+public class AttribList
+{
+	public List<Attribute> attributes;
+}
