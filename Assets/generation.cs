8c703087aeabf8c119f92394e357526f1382b8f7
Xygar <48291956+Xygar@users.noreply.github.com>
2020-08-11 15:37:11 +0200

    generationPerdue

--- Assets/generation.cs
+++ Assets/generation.cs
@@ -7,20 +7,57 @@ public class generation : MonoBehaviour
 {
 	string path;
 	string jsonString;
+	
+	List<Attribute> noms;
+	List<Attribute> religions;
+	List<Attribute> sexualites;
+	List<Attribute> sexes;
+	List<Attribute> applications;
+	List<Attribute> couleurs;
+	List<Attribute> ethnies;
+	List<Attribute> fidels;
+
+	string prenom;
+	string nomFamille;
+	string religion;
+	string sexualite;
+	string sexe;
+	string application;
+	string couleur;
+	string ethnie;
+	string fidel;
+	int scorePerso;
+	
 		// Start is called before the first frame update
 		void Start()
-		{
-			path = Application.dataPath + "/application.json";
-			jsonString = File.ReadAllText(path);
-			Debug.Log(jsonString);
-			AttribList list = JsonUtility.FromJson<AttribList> (jsonString);
-			Debug.Log(list.attribute[0].nom);
+		{			
+			new Random
+		
+			noms = Telecharge("/nom.json");
+			religions = Telecharge("/religion.json");
+			sexualites = Telecharge("/sexualite.json");
+			sexes = Telecharge("/sexe.json");
+			professions = Telecharge("/proffession.json");
+			applications = Telecharge("/application.json");
+			couleurs = Telecharge("/couleur.json");
+			ethnies = Telecharge("/ethnie.json");
+			fidels = Telecharge("/fidel.json");
 		}
 
 	// Update is called once per frame
 	void Update()
 	{
 
+	}
+	
+	void generationPerdue(){
+		
+	}
+	
+	List<Attribute> Telecharge(string path){
+		jsonString = File.ReadAllText(path);
+		AttribList list = JsonUtility.FromJson<AttribList> (jsonString);
+		return list.attribute;
 	}
 }
 
