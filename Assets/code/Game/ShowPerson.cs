48cd1c9c9297c2c871604c5ab33cc317a9e567f9
loic.jurasz <loic.jurasz@he-arc.ch>
2020-08-11 11:41:46 +0200

    gitignore and start of project

---
+++ Assets/code/Game/ShowPerson.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPerson : MonoBehaviour
+{
+    public GameObject circle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(circle, new Vector3(0.208757f, -0.2164043f, -6.712757f), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
